b97ab05ffbfe00d0ae2a0441a6775cc6423e3531
Lucas Dayries <Lucas Dayries>
2016-03-19 14:17:41 +0100

    wtf

--- FollowUp/app/src/main/java/followup/followup/activity/FirstTimeActivity.java
+++ FollowUp/app/src/main/java/followup/followup/activity/FirstTimeActivity.java
@@ -1,7 +1,20 @@
 package followup.followup.activity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import followup.followup.R;
+
 /**
  * Created by Steeven on 19/03/2016.
  */
-public class FirstTimeActivity {
+public class FirstTimeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time);
+    }
 }
