3e6b5081686a7937fea7fe7534e985f6805d1349
Lucas Dayries <Lucas Dayries>
2016-03-19 20:15:42 +0100

    first activity et questionaire

--- FollowUp/app/src/main/java/followup/followup/activity/FirstTimeActivity.java
+++ FollowUp/app/src/main/java/followup/followup/activity/FirstTimeActivity.java
@@ -1,7 +1,12 @@
 package followup.followup.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import followup.followup.R;
 
@@ -16,5 +21,15 @@ public class FirstTimeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time);
+
+        Button buttonPillule = (Button) findViewById(R.id.buttonPillule);
+        final FragmentManager fm = getFragmentManager();
+        final android.app.Fragment fragment = fm.findFragmentById(R.id.first_pillule_setting_view);
+
+        buttonPillule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
