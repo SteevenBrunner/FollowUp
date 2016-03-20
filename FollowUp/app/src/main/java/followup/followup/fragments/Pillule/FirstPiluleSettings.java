e5b1c558cb22c4d3b359923fcbec060e61ca9bf1
Lucas Dayries <Lucas Dayries>
2016-03-20 06:15:21 +0100

    kom sam soul

--- FollowUp/app/src/main/java/followup/followup/fragments/Pillule/FirstPiluleSettings.java
+++ FollowUp/app/src/main/java/followup/followup/fragments/Pillule/FirstPiluleSettings.java
@@ -1,21 +1,29 @@
 package followup.followup.fragments.Pillule;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import followup.followup.R;
+import followup.followup.activity.MainActivity;
 
 /**
  * Created by Steeven on 19/03/2016.
  */
 public class FirstPiluleSettings extends Fragment
 {
+    Activity context;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        context = getActivity();
         return inflater.inflate(R.layout.first_pillule_setting_view, container, false);
     }
 }
--- FollowUp/app/src/main/java/followup/followup/fragments/Pillule/UrgenceFragment.java
+++ FollowUp/app/src/main/java/followup/followup/fragments/Pillule/UrgenceFragment.java
@@ -1,22 +1,51 @@
 package followup.followup.fragments.Pillule;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import followup.followup.R;
+import followup.followup.activity.MainActivity;
+import android.app.Fragment;
 
 /**
  * Created by Steeven on 19/03/2016.
  */
-public class UrgenceFragment extends Fragment {
+public class UrgenceFragment extends Fragment /*implements View.OnClickListener */  {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_urgence, null);
+        final Button buttonPillule = (Button) v.findViewById(R.id.buttonOubli);
         return inflater.inflate(R.layout.fragment_urgence, container, false);
     }
+
+/*    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.buttonOubli:
+                Fragment newFragment = new UrgenceOubli_recapFragment();
+                Log.d("=============", "TEST ON CLICKEZAEA");
+                // consider using Java coding conventions (upper first char class names!!!)
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack
+                transaction.replace(R.id.fragment_container, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+        }
+    }*/
 
 }
